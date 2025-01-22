92cde34c7db021aec9ec2f0294a81ed5f7173237
Bhaskar <bhaskarkotakonda123@gmail.com>
2025-01-22 21:10:10 +0530

    initial

---
+++ javascript/array.js
@@ -0,0 +1,9 @@
+let fruits=['apple','banana','cherry','sapota','apple'];
+console.log(fruits.length);
+console.log(fruits.at(2));
+console.log(fruits.join("#"));
+console.log(fruits.pop());
+console.log(fruits.pop(2));
+console.log(fruits);
+console.log(fruits.push('mango'));
+console.log(fruits);
