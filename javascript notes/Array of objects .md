e85453c8b7cbb9f568c7dcb8c4e14f1af4e912f2
Bhaskar <bhaskarkotakonda123@gmail.com>
2025-01-24 10:36:05 +0530

    initial

---
+++ javascript notes/Array of objects .md
@@ -0,0 +1,4 @@
+# Array of objects:
+- it is structured data storage.
+- it is the combination of both arrays and objects 
+- easy to iterate over
---
+++ javascript/AOB.js
@@ -0,0 +1,33 @@
+// const users = [
+//     { id: 1, name: "alice", age:21},
+//     { id: 2, name: "bhaskar", age:22},
+//     { id: 3, name: "basu", age:23}
+// ];
+
+
+
+// // easy to iterate over:
+
+// users.forEach(user => console.log(user.name));
+
+
+// // flexibility with properties:
+
+// const products = [
+//     {id:1, name:"basu",price:2000}
+// ]
+
+
+
+
+const students = [
+    { id: 1, name: "alice", age:21},
+    { id: 2, name: "bhaskar", age:22},
+    { id: 3, name: "basu", age:23}
+];
+
+console.log(students[0].name);
+console.log(students[1].['age']);
+
+
+
