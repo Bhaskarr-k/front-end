c56bcfbba9b0bcae5eb302db729da1a205f25166
Bhaskar <bhaskarkotakonda123@gmail.com>
2025-01-27 21:19:32 +0530

    initial

--- javascript/dom.js
+++ javascript/dom.js
@@ -1,17 +1,36 @@
-const header = document.getElementById("header")
-console.log(header.textContent);
+// const header = document.getElementById("header")
+// console.log(header.textContent);
 
-// if you we have multiple classes then you can use 
+// const footer = document.getElementById("footer")
+// console.log(footer.textContent);
 
-const menus = document.getElementsByClassName("menus")
-Array.from(menus).foreach(div => console.log(div.textcontent))
+// const first = document.getElementById("first")
+// console.log(first.textContent);
+
+// const seconds = document.getElementsByClassName("second")
+// Array.from(seconds).forEach(second => console.log(second.textContent));
+
+// const three = document.getElementsByClassName("thrid")
+// Array.from(three).forEach(thrid => console.log(thrid.textContent));
+
+// const four = document.getElementById("four")
+// console.log(four.textContent);
+
+// const once = document.getElementsByClassName("one")
+// Array.from(once).forEach(once => console.log(one.textContent));
+
+// const para = document.getElementsByTagName("div")
+// Array.from(para).forEach(div => console.log(div.textContent));
 
 
-// if you want to select query selector
+// const six = document.getElementById("six")
+// console.log(six.textContent);
 
-const firstbox = document.querySelector('.box')
-console.log(firstbox.textContent);
+// const seven = document.getElementsByClassName("basu")
+// Array.from(seven).forEach(basu => console.log(basu.textContent))
 
+// const sevens = document.getElementsByTagName("div")
+// Array.from(sevens).forEach(div => console.log(div.textContent));
 
-const item = document.querySelectorALl('.box')
-items.forEach(item => console.log(item.textContent))
+const card = document.getElementById("card1")
+console.log(card.matches('.card'));
