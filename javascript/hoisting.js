6f7d6b1bc2c6c8e610c0ea3e0ddde71e758a230c
Bhaskar <bhaskarkotakonda123@gmail.com>
2025-05-02 11:25:15 +0530

    initial

---
+++ javascript/hoisting.js
@@ -0,0 +1,32 @@
+// sayHello(); 
+
+// function sayHello() {
+//     console.log("Hello");
+// }
+
+// // default parameters
+// function greet(user = "Guest") {
+//     console.log(`Hello, ${user}`);
+// }
+
+// spread  
+// copying array
+// let a=[1,2,3];
+// let b=[...a];
+// console.log(b);
+// merging array
+
+// let a =[1,2,3];
+// let b= [4,5,6]
+// let c =[...a,...b]
+// console.log(c)
+
+// array destructuring
+const colors = ['red','green','yellow']
+const [first, second] = colors;
+const [,,third] = colors
+
+console.log(first);
+console.log(second);
+console.log(third);
+
