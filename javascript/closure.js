ea60ad0ab154d96ef64f9a87746a9ad459ea4ac1
Bhaskar <bhaskarkotakonda123@gmail.com>
2025-05-06 16:55:41 +0530

    initial

---
+++ javascript/closure.js
@@ -0,0 +1,30 @@
+// function outer() {
+//     let count = 0;
+    
+//     function inner() {
+//       count++;
+//       console.log(count);
+//     }
+  
+//     return inner;
+//   }
+  
+//   const counter = outer();
+//   counter(); 
+//   counter(); 
+//   counter(); 
+  
+// closure:
+// function outer() {
+//     let count = 0;
+
+//     function inner() {
+//         count++;
+//         console.log(count);
+//     }
+//     return inner;
+// }
+// const Counter = outer();
+// Counter();
+
+
